2d27c046f8fd6f4269ac397d40e4eff573fb8a87
mercy-brice-kamilindi <mercy.brice.kamilindi@stu.ocsb.ca>
2025-12-17 12:26:43 -0500

    rock paper scissers

--- main.ts
+++ main.ts
@@ -1,8 +1,55 @@
 /* Copyright (c) 2020 MTHS All rights reserved
  *
- * Created by: XXX
- * Created on: Sep 2020
- * This program ...
+ * Created by:Mercy
+ * Created on:oct 2025
+ * This program does rock paper scissors
 */
 
-basic.showString('Hello, World!')
+// variables
+let randomNumber: number = -1
+let score: number = 0
+
+basic.clearScreen()
+basic.showIcon(IconNames.Happy)
+
+input.onGesture(Gesture.Shake, function () {
+    randomNumber = randint(0, 2)
+    basic.clearScreen()
+
+    // Scissors
+    if (randomNumber == 0) {
+        basic.showIcon(IconNames.Scissors)
+    }
+
+    // Paper
+    if (randomNumber == 1) {
+        basic.showIcon(IconNames.Square)
+    }
+
+    // Rock
+    if (randomNumber == 2) {
+        basic.showIcon(IconNames.SmallSquare)
+    }
+
+    // Pause and get ready for another
+    basic.pause(5000)
+    basic.showIcon(IconNames.Happy)
+})
+
+// add score
+input.onButtonPressed(Button.A, function () {
+    basic.clearScreen()
+    basic.showIcon(IconNames.Yes)
+    score += 1
+    basic.pause(1000)
+    basic.showIcon(IconNames.Happy)
+})
+
+// show score
+input.onButtonPressed(Button.B, function () {
+    basic.clearScreen()
+    basic.showString("Score:")
+    basic.showNumber(score)
+    basic.clearScreen()
+    basic.showIcon(IconNames.Happy)
+})
